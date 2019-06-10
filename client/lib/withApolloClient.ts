8be76d10c978ea7e7d241d33e4c821d78d13872e
Jonathan Penton <jonathan@penton.site>
2019-06-10 11:32:19 -0500

    API url change

--- client/lib/withApolloClient.ts
+++ client/lib/withApolloClient.ts
@@ -6,9 +6,7 @@ export default (isProd: () => boolean) =>
   withApollo(
     ({ ctx, headers, initialState }) =>
       new ApolloClient({
-        uri: isProd()
-          ? 'https://gitgud-api.penton.site'
-          : 'http://localhost:8000',
+        uri: isProd() ? 'https://gitgud-api.cpu.tf' : 'http://localhost:8000',
         cache: new InMemoryCache().restore(initialState || {}),
       }),
   );
